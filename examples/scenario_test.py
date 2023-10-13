c1ca83ec55a03699da5873600d053eece02db9a3
Timothy Arnett <tim.arnett@psibernetix.com>
2023-10-13 12:40:51 -0400

    Changed test scenario and controller to use/test mines

--- examples/scenario_test.py
+++ examples/scenario_test.py
@@ -13,8 +13,8 @@ from graphics_both import GraphicsBoth
 my_test_scenario = Scenario(name='Test Scenario',
                             num_asteroids=10,
                             ship_states=[
-                                {'position': (400, 400), 'angle': 90, 'lives': 3, 'team': 1},
-                                {'position': (400, 600), 'angle': 90, 'lives': 3, 'team': 2},
+                                {'position': (400, 400), 'angle': 90, 'lives': 3, 'team': 1, "mines_remaining": 100},
+                                # {'position': (400, 600), 'angle': 90, 'lives': 3, 'team': 2},
                             ],
                             map_size=(1000, 800),
                             time_limit=60,
--- examples/test_controller.py
+++ examples/test_controller.py
@@ -11,7 +11,7 @@ class TestController(KesslerController):
     def __init__(self):
         self.eval_frames = 0
 
-    def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool]:
+    def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool, bool]:
         """
         Method processed each time step by this controller.
         """
@@ -19,9 +19,13 @@ class TestController(KesslerController):
         thrust = 0
         turn_rate = 90
         fire = True
+        drop_mine = False
+        dropped_mine = False
+        if game_state["time"] >= 1.0 and not len(game_state["mines"]) > 0:
+            drop_mine = True
         self.eval_frames +=1
 
-        return thrust, turn_rate, fire
+        return thrust, turn_rate, fire, drop_mine
 
     @property
     def name(self) -> str:
