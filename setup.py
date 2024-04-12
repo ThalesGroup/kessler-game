b86769773c1b45450b7d742646c14ea8e5ea25a8
Tim Arnett <tim.arnett@defense.us.thalesgroup.com>
2024-04-12 17:48:48 -0400

    Reverting setup.py to just mypyc version

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@
 # this source code package.
 
 from setuptools import setup, find_packages
-import os
+from mypyc.build import mypycify
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
@@ -18,51 +19,42 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-if not bool(int(os.getenv('KESSLER_SKIP_COMPILE', '0'))):
-    from mypyc.build import mypycify
+# List of all Python modules to compile with MyPyC
+mypyc_modules = [
+    "src/kesslergame/asteroid.py",
+    "src/kesslergame/bullet.py",
+    "src/kesslergame/collisions.py",
+    "src/kesslergame/controller.py",
+    "src/kesslergame/controller_gamepad.py",
+    "src/kesslergame/kessler_game.py",
+    "src/kesslergame/mines.py",
+    "src/kesslergame/scenario.py",
+    "src/kesslergame/score.py",
+    "src/kesslergame/ship.py",
+    "src/kesslergame/team.py",
+    "src/kesslergame/graphics/graphics_base.py",
+    "src/kesslergame/graphics/graphics_handler.py",
+    "src/kesslergame/graphics/graphics_plt.py",
+    "src/kesslergame/graphics/graphics_tk.py",
+    "src/kesslergame/graphics/graphics_ue.py",
+    # Add __init__.py if you have specific initialization code that needs compilation.
+    # "src/__init__.py",
+    "src/kesslergame/__init__.py",
+    "src/kesslergame/graphics/__init__.py",
+]
 
-    # List of all Python modules to compile with MyPyC
-    mypyc_modules = [
-        "src/kesslergame/asteroid.py",
-        "src/kesslergame/bullet.py",
-        "src/kesslergame/collisions.py",
-        "src/kesslergame/controller.py",
-        "src/kesslergame/controller_gamepad.py",
-        "src/kesslergame/kessler_game.py",
-        "src/kesslergame/mines.py",
-        "src/kesslergame/scenario.py",
-        "src/kesslergame/score.py",
-        "src/kesslergame/ship.py",
-        "src/kesslergame/team.py",
-        "src/kesslergame/graphics/graphics_base.py",
-        "src/kesslergame/graphics/graphics_handler.py",
-        "src/kesslergame/graphics/graphics_plt.py",
-        "src/kesslergame/graphics/graphics_tk.py",
-        "src/kesslergame/graphics/graphics_ue.py",
-        # Add __init__.py if you have specific initialization code that needs compilation.
-        # "src/__init__.py",
-        "src/kesslergame/__init__.py",
-        "src/kesslergame/graphics/__init__.py",
-    ]
+setup(
+    name='KesslerGame',
+    version=verstr,
+    packages=find_packages(where='src', exclude=['examples', 'src.examples', '*.examples.*', 'examples.*']),
+    install_requires=requirements,
+    ext_modules=mypycify(mypyc_modules),
+    package_data={
+        '': ['*.png'],
+    },
+    package_dir={'': 'src'},
+)
 
-    setup(
-        name='KesslerGame',
-        version=verstr,
-        packages=find_packages(where='src', exclude=['examples', 'src.examples', '*.examples.*', 'examples.*']),
-        install_requires=requirements,
-        ext_modules=mypycify(mypyc_modules),
-        package_data={
-            '': ['*.png'],
-        },
-        package_dir={'': 'src'},
-    )
-else:
-    # This branch doesn't use mypyc compilation
-    setup(
-        name='KesslerGame',
-        version=verstr,
-        install_requires=requirements,
-    )
 
 
 
