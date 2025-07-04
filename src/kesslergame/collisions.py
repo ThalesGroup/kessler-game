66ced7c7c65890f76ae00a2ae742a57c5a39f7d1
Jie-F <78284005+Jie-F@users.noreply.github.com>
2025-07-04 14:23:12 -0600

    Add back old collision check

--- src/kesslergame/collisions.py
+++ src/kesslergame/collisions.py
@@ -114,3 +114,28 @@ def circle_line_collision_continuous(
         return True
     '''
     return False
+
+def circle_line_collision(line_A: tuple[float, float], line_B: tuple[float, float], center: tuple[float, float], radius: float) -> bool:
+    # Old collision check, which was discrete, and also had false positives:
+    
+    # Check if circle edge is within the outer bounds of the line segment (offset for radius)
+    # Not 100% accurate (some false positives) but fast and rare inaccuracies
+    x_bounds = [min(line_A[0], line_B[0]) - radius, max(line_A[0], line_B[0]) + radius]
+    if center[0] < x_bounds[0] or center[0] > x_bounds[1]:
+        return False
+    y_bounds = [min(line_A[1], line_B[1]) - radius, max(line_A[1], line_B[1]) + radius]
+    if center[1] < y_bounds[0] or center[1] > y_bounds[1]:
+        return False
+
+    # calculate side lengths of triangle formed from the line segment and circle center point
+    a = math.dist(line_A, center)
+    b = math.dist(line_B, center)
+    c = math.dist(line_A, line_B)
+
+    # Heron's formula to calculate area of triangle and resultant height (distance from circle center to line segment)
+    s = 0.5 * (a + b + c)
+
+    cen_dist = 2.0 / c * math.sqrt(max(0.0, s * (s-a) * (s-b) * (s-c)))
+
+    # If circle distance to line segment is less than circle radius, they are colliding
+    return cen_dist < radius
